673a69a98a9ae5f6dc905051588153df39e4d86a
arhn3487 <arhasan3487@gmail.com>
2026-05-18 01:03:38 +0600

    Initial commit: Completed Spring Boot setup and PostgreSQL connection

---
+++ src/main/java/com/example/interndemo/InterndemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.interndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterndemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterndemoApplication.class, args);
+    }
+
+}
